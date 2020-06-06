1ade39335db7a2df48b0b6aa75bd9347434c48f2
pitazzo <pedromalo@live.com>
2020-06-06 13:29:44 +0200

    add: minor changes

--- frontend-user/lib/ui/screens/reservation_info_screen.dart
+++ frontend-user/lib/ui/screens/reservation_info_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ucampus_lib/core/models/reservation.dart';
-import 'package:ucampus_lib/ui/widgets/reservation/reservation_info_card.dart';
 import 'package:ucampus/core/redux/connectors/reservations/reservation_info.dart';
 
 class ReservationInfoScreen extends StatelessWidget {
