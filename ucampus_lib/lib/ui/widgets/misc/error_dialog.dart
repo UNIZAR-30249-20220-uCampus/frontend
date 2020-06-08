89d6c8f084a41d9bb222aedab966598de5062dad
pitazzo <pedromalo@live.com>
2020-06-08 15:24:13 +0200

    add: error dialog

---
+++ ucampus_lib/lib/ui/widgets/misc/error_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
+
+void showUserExceptionDialog(
+  BuildContext context,
+  UserException userException,
+) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      backgroundColor: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      title: Text(
+        'Algo ha fallado',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      content: Text(userException.msg.replaceAll("Unhandled Failure ", "")),
+      actions: <Widget>[
+        FlatButton(
+          child: Text(
+            "ACEPTAR",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ],
+    ),
+  );
+}
